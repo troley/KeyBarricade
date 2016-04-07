76d9252093119d4ed97485937cfe0f3f55f44ab4
Troley <Troley@RENE-W10>
2016-04-07 21:07:36 +0200

    klaar voor vertrek

--- test/keybarricade/PlayerTest.java
+++ test/keybarricade/PlayerTest.java
@@ -92,7 +92,7 @@ public class PlayerTest {
         int y = 0;
         Floor f = new Floor(35, 0);
         Player p = new Player(0, 0);
-        if (!p.checkCollision(f, KeyEvent.VK_RIGHT)) { // should block nothing, player wants to move onto a floor tile
+        if (!p.checkCollision(f, KeyEvent.VK_RIGHT)) { // should block nothing, player should move onto a floor tile
             p.move(x, y);
         }
 
@@ -109,7 +109,7 @@ public class PlayerTest {
         int y = 0;
         Key k = new Key(35, 0, ID.Object100);
         Player p = new Player(0, 0);
-        if (!p.checkCollision(k, KeyEvent.VK_RIGHT)) { // should block nothing, player wants to move onto a key tile
+        if (!p.checkCollision(k, KeyEvent.VK_RIGHT)) { // should block nothing, player should move onto a key tile
             p.move(x, y);
         }
 
@@ -126,7 +126,7 @@ public class PlayerTest {
         int y = 0;
         Finish f = new Finish(35, 0);
         Player p = new Player(0, 0);
-        if (!p.checkCollision(f, KeyEvent.VK_RIGHT)) { // should block nothing, player wants to move onto a finish tile
+        if (!p.checkCollision(f, KeyEvent.VK_RIGHT)) { // should block nothing, player should move onto a finish tile
             p.move(x, y);
         }
 
@@ -134,5 +134,109 @@ public class PlayerTest {
         int uitkomst = p.getX();
 
         assertEquals(verwachteUitkomstX, uitkomst);
+    }
+    
+    /*
+     * From testMove6 on, these are the same tests, but for the Y position.
+     */
+    
+    @Test
+    public void testMove6() {
+        System.out.println("move");
+        int x = 0;
+        int y = 35;
+        Player p = new Player(0, 0);
+        p.move(x, y);
+
+        int verwachteUitkomstY = 35;
+        int uitkomst = p.getY();
+
+        assertEquals(verwachteUitkomstY, uitkomst);
+    }
+    
+    
+    @Test
+    public void testMove7() {
+        System.out.println("move against a wall");
+        int x = 0;
+        int y = 35;
+        Wall w = new Wall(0, 35);
+        Player p = new Player(0, 0);
+        if (!p.checkCollision(w, KeyEvent.VK_DOWN)) { // should block the player from moving because of wall collision
+            p.move(x, y);
+        }
+
+        int verwachteUitkomstY = 0; // 0 because player should not be allowed to move onto a wall
+        int uitkomst = p.getY();
+
+        assertEquals(verwachteUitkomstY, uitkomst);
+    }
+    
+    @Test
+    public void testMove8() {
+        System.out.println("move against a barricade");
+        int x = 0;
+        int y = 35;
+        Barricade b = new Barricade(0, 35, ID.Object100);
+        Player p = new Player(0, 0);
+        if (!p.checkCollision(b, KeyEvent.VK_DOWN)) { // should block the player from moving because of barricade collision
+            p.move(x, y);
+        }
+
+        int verwachteUitkomstY = 0; // 0 because player should not be allowed to move onto a barricade
+        int uitkomst = p.getY();
+
+        assertEquals(verwachteUitkomstY, uitkomst);
+    }
+    
+    @Test
+    public void testMove9() {
+        System.out.println("move onto a floor tile");
+        int x = 0;
+        int y = 35;
+        Floor f = new Floor(0, 35);
+        Player p = new Player(0, 0);
+        if (!p.checkCollision(f, KeyEvent.VK_DOWN)) { // should block nothing, player should move onto a floor tile
+            p.move(x, y);
+        }
+
+        int verwachteUitkomstY = 35;
+        int uitkomst = p.getY();
+
+        assertEquals(verwachteUitkomstY, uitkomst);
+    }
+    
+    @Test
+    public void testMove10() {
+        System.out.println("move onto a key tile");
+        int x = 0;
+        int y = 35;
+        Key k = new Key(0, 35, ID.Object100);
+        Player p = new Player(0, 0);
+        if (!p.checkCollision(k, KeyEvent.VK_DOWN)) { // should block nothing, player should move onto a key tile
+            p.move(x, y);
+        }
+
+        int verwachteUitkomstY = 35;
+        int uitkomst = p.getY();
+
+        assertEquals(verwachteUitkomstY, uitkomst);
+    }
+    
+    @Test
+    public void testMove11() {
+        System.out.println("move onto a finish tile");
+        int x = 0;
+        int y = 35;
+        Finish f = new Finish(0, 35);
+        Player p = new Player(0, 0);
+        if (!p.checkCollision(f, KeyEvent.VK_DOWN)) { // should block nothing, player should move onto a finish tile
+            p.move(x, y);
+        }
+
+        int verwachteUitkomstY = 35;
+        int uitkomst = p.getY();
+
+        assertEquals(verwachteUitkomstY, uitkomst);
     }
 }
